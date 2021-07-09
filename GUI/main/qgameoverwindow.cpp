ff91dd859befd2434914f659f37a2bb016f89472
1538741602 <48423345+1538741602@users.noreply.github.com>
2021-07-09 21:57:51 +0800

    Create qgameoverwindow.cpp

---
+++ GUI/main/qgameoverwindow.cpp
@@ -0,0 +1,32 @@
+#include "qgameoverwindow.h"
+#include "qresetbutton.h"
+
+#include <QVBoxLayout>
+#include <QLabel>
+#include <QDebug>
+#include <QResizeEvent>
+
+QGameOverWindow::QGameOverWindow(QWidget *parent) :
+    QWidget(parent)
+{
+    setStyleSheet("QGameOverWindow { background: rgb(84, 199, 160); }");
+    setFixedSize(425,205);
+    QVBoxLayout *layout = new QVBoxLayout(this);
+    // game over label
+    QLabel* gameover = new QLabel("Game Over!", this);
+    gameover->setStyleSheet("QLabel { color: rgb(51, 122, 99); font: 40pt; font: bold;} ");
+    // reset button
+    reset = new QResetButton(this);
+    reset->setFixedHeight(50);
+    reset->setFixedWidth(100);
+    // add game over label to window
+    layout->insertWidget(0,gameover,0,Qt::AlignCenter);
+    // add reset button to window
+    layout->insertWidget(1,reset,0,Qt::AlignCenter);
+}
+
+QResetButton* QGameOverWindow::getResetBtn() const
+{
+    return reset;
+}
+               
